cf9cad77e6612a924babfaf201b68412d26e33c0
Zeno Rocha <zeno.rocha@liferay.com>
2013-10-26 23:42:24 +0200

    Add softwares installation script using homebrew-cask - Fixes #6

---
+++ apps.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# homebrew-cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# backup
+brew cask install cloudapp
+brew cask install dropbox
+
+# browser
+brew cask install firefox-aurora
+brew cask install google-chrome-canary
+brew cask install opera-next
+
+# calendar
+brew cask install fantastical
+
+# documentation lookup
+brew cask install dash
+
+# ftp
+brew cask install filezilla
+
+# gif record
+brew cask install licecap
+
+# git
+brew cask install sourcetree
+
+# irc
+brew cask install lime-chat
+
+# markdown edit
+brew cask install mou
+
+# notes
+brew cask install evernote
+
+# passwords
+brew cask install one-password
+
+# productivity
+brew cask install alfred
+
+# sleep
+brew cask install caffeine
+
+# terminal
+brew cask install iterm2
+
+# torrent
+brew cask install u-torrent
+
+# video call
+brew cask install skype
+
+# virtual machine
+brew cask install virtualbox
+
+# windows
+brew cask install divvy
+
+# zip, rar, etc
+brew cask install the-unarchiver
