c34a2d2ebfe5a0cb535be91fede0c109fa9fa841
Zeno Rocha <zeno.rocha@liferay.com>
2013-10-27 11:30:45 +0100

    Remove Z plugin since its already contained on oh-my-zsh

--- z/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source $HOME/.dotfiles/log.sh
-
-if [[ -d "$HOME/.z" ]]; then
-	msg_checking "z"
-else
-	msg_install "z" "git clone https://github.com/rupa/z.git $HOME/.z"
-	git clone https://github.com/rupa/z.git $HOME/.z
-fi
