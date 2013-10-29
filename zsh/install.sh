bebd8536225414f7d3420ac967f12780ff365670
Zeno Rocha <zeno.rocha@liferay.com>
2013-10-29 12:32:43 +0100

    Improve zshrc operations

--- zsh/install.sh
+++ zsh/install.sh
@@ -12,13 +12,13 @@ fi
 # Setting ZSH as your default shell
 chsh -s /bin/zsh
 
-# Backing up
-if [[ -d "$HOME/.zshrc" || -f "$HOME/.zshrc" ]]; then
-	mv $HOME/.zshrc $HOME/.zshrc_bkp
+# -- ZSH Config ----------------------------------------------------------------
+if [[ -d "$HOME/.zshrc" || -L "$HOME/.zshrc" ]]; then
+	msg_ok "zshrc"
+else
+	msg_run "zshrc" "ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc"
+	ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 fi
-
-# Linking ~/.zshrc to ~/.dotfiles/zsh/.zshrc
-ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 
 # -- Oh my ZSH -----------------------------------------------------------------
 if [[ -d "$HOME/.oh-my-zsh" ]]; then
