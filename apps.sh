a59edcf2337aedd640378ce24d1013aa7164a6b2
Zeno Rocha <zeno.rocha@liferay.com>
2013-11-05 13:10:06 -0800

    Remove apps from App Store

--- apps.sh
+++ apps.sh
@@ -5,19 +5,12 @@ brew tap phinze/homebrew-cask
 brew install brew-cask
 
 # backup
-brew cask install cloudapp
 brew cask install dropbox
 
 # browser
 brew cask install firefox-aurora
 brew cask install google-chrome-canary
 brew cask install opera-next
-
-# calendar
-brew cask install fantastical
-
-# documentation lookup
-brew cask install dash
 
 # ftp
 brew cask install filezilla
@@ -33,9 +26,6 @@ brew cask install lime-chat
 
 # markdown edit
 brew cask install mou
-
-# notes
-brew cask install evernote
 
 # passwords
 brew cask install one-password
@@ -57,9 +47,3 @@ brew cask install skype
 
 # virtual machine
 brew cask install virtualbox
-
-# windows
-brew cask install divvy
-
-# zip, rar, etc
-brew cask install the-unarchiver
